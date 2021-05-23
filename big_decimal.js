ea9950cd6692aabbc720f801283fb91e9267ee69
Wachira Duangdee <wcrcorp@gmail.com>
2021-05-24 02:34:54 +0700

    Create big_decimal.js

---
+++ big_decimal.js
@@ -0,0 +1,11 @@
+const Big = require('big.js');
+
+var val = new Big(0.0);
+var amount = new Big(2.55);
+
+var sum = val.plus(amount).times(100000);
+
+console.log(sum.toFixed(2));
+
+// $ nodejs big_decimal.js 
+// 255000.00
