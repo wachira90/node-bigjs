8f9921e147a9a161c28515d80de87fc4b3137623
Wachira Duangdee <wcrcorp@gmail.com>
2021-05-24 02:33:52 +0700

    Create count_currency.js

---
+++ count_currency.js
@@ -0,0 +1,13 @@
+var sum = 0;
+// two euros fifty-five cents
+var amount = 2.55;
+
+for (let i = 0; i < 100000; i++) {
+
+    sum += amount;
+}
+
+console.log(sum);
+
+//$ nodejs numbers.js 
+//254999.9999995398
